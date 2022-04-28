8f203bcf1160f073a0fa9bb0e6a41b13512cfbb2
Shahrooz Sabet <shahrooz.sabet@ist.com>
2022-04-28 19:34:49 +0200

    AppBar ElevationScroll added.

--- src/components/ui/Header.js
+++ src/components/ui/Header.js
@@ -1,42 +1,24 @@
 import React from "react";
-import {AppBar, makeStyles, Toolbar, useScrollTrigger, Zoom} from "@material-ui/core";
+import {AppBar, Toolbar, useScrollTrigger} from "@material-ui/core";
 
-const useStyles = makeStyles((theme) => ({
-    root: {
-        position: 'fixed', bottom: theme.spacing(2), right: theme.spacing(2),
-    },
-}));
-
-function ScrollTop(props) {
+function ElevationScroll(props) {
     const {children} = props;
-    const classes = useStyles();
-
     const trigger = useScrollTrigger({
-        disableHysteresis: true, threshold: 100,
+        disableHysteresis: true, threshold: 0,
     });
 
-    const handleClick = (event) => {
-        const anchor = (event.target.ownerDocument || document).querySelector('#back-to-top-anchor');
-
-        if (anchor) {
-            anchor.scrollIntoView({behavior: 'smooth', block: 'center'});
-        }
-    };
-
-    return (<Zoom in={trigger}>
-        <div onClick={handleClick} role="presentation" className={classes.root}>
-            {children}
-        </div>
-    </Zoom>);
+    return React.cloneElement(children, {
+        elevation: trigger ? 4 : 0,
+    });
 }
 
 
 export function Header(props) {
-    return (<ScrollTop>
+    return (<ElevationScroll>
         <AppBar position={"fixed"}>
             <Toolbar>
                 Arc Development
             </Toolbar>
         </AppBar>
-    </ScrollTop>)
+    </ElevationScroll>)
 }
