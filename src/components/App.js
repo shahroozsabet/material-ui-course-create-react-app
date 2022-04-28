4f267eb104999d62c800cf2a211dffd26d9a8389
Shahrooz Sabet <shahrooz.sabet@ist.com>
2022-04-28 18:58:42 +0200

    AppBar ScrollTop added.

--- src/components/App.js
+++ src/components/App.js
@@ -4,7 +4,14 @@ import {Header} from "./ui/Header";
 function App() {
     return (<div className="App">
         <Header/>
-        Hello!
+        {[...new Array(48)]
+            .map(
+                () => `Cras mattis consectetur purus sit amet fermentum.
+Cras justo odio, dapibus ac facilisis in, egestas eget quam.
+Morbi leo risus, porta ac consectetur ac, vestibulum at eros.
+Praesent commodo cursus magna, vel scelerisque nisl consectetur et.`,
+            )
+            .join('\n')}
     </div>);
 }
 
--- src/components/ui/Header.js
+++ src/components/ui/Header.js
@@ -1,9 +1,42 @@
-import {AppBar, Toolbar} from "@material-ui/core";
+import React from "react";
+import {AppBar, makeStyles, Toolbar, useScrollTrigger, Zoom} from "@material-ui/core";
+
+const useStyles = makeStyles((theme) => ({
+    root: {
+        position: 'fixed', bottom: theme.spacing(2), right: theme.spacing(2),
+    },
+}));
+
+function ScrollTop(props) {
+    const {children} = props;
+    const classes = useStyles();
+
+    const trigger = useScrollTrigger({
+        disableHysteresis: true, threshold: 100,
+    });
+
+    const handleClick = (event) => {
+        const anchor = (event.target.ownerDocument || document).querySelector('#back-to-top-anchor');
+
+        if (anchor) {
+            anchor.scrollIntoView({behavior: 'smooth', block: 'center'});
+        }
+    };
+
+    return (<Zoom in={trigger}>
+        <div onClick={handleClick} role="presentation" className={classes.root}>
+            {children}
+        </div>
+    </Zoom>);
+}
+
 
 export function Header(props) {
-    return (<AppBar position={"fixed"}>
-        <Toolbar>
-            Arc Development
-        </Toolbar>
-    </AppBar>)
+    return (<ScrollTop>
+        <AppBar position={"fixed"}>
+            <Toolbar>
+                Arc Development
+            </Toolbar>
+        </AppBar>
+    </ScrollTop>)
 }
