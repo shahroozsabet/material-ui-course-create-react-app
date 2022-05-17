c40cc1f9e62c0a86acf9049f3a18b5de51043637
Shahrooz Sabet <shahrooz.sabet@ist.com>
2022-05-17 11:57:44 +0200

    customSoftware added.

--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -1,9 +1,10 @@
 import React from "react";
 import Lottie from 'react-lottie'
-import {Button, Grid, makeStyles, Typography, useTheme} from "@material-ui/core";
+import {Button, Grid, makeStyles, Typography, useMediaQuery, useTheme} from "@material-ui/core";
 
 import animationData from '../animations/landinganimation/data'
 import ButtonArrow from "./ui/ButtonArrow";
+import customSoftwareIcon from "../assets/Custom Software Icon.svg"
 
 const useStyles = makeStyles(theme => ({
     animation: {
@@ -23,16 +24,11 @@ const useStyles = makeStyles(theme => ({
     }, buttonContainer: {
         marginTop: "1em"
     }, learnButtonHero: {
-        borderColor: theme.palette.common.blue,
-        color: theme.palette.common.blue,
-        borderWidth: 2,
-        textTransform: "none",
-        borderRadius: 50,
-        fontFamily: "Roboto",
-        fontWeight: "bold",
-        fontSize: "0.9rem",
-        height: 45,
-        width: 145
+        ...theme.typography.learnButton, fontSize: "0.9rem", height: 45, width: 145
+    }, learnButton: {
+        ...theme.typography.learnButton, fontSize: "0.7rem", height: 35, padding: 5, [theme.breakpoints.down("sm")]: {
+            marginBottom: "2em"
+        }
     }, mainContainer: {
         marginTop: "5em", [theme.breakpoints.down("md")]: {
             marginTop: "3em"
@@ -43,12 +39,25 @@ const useStyles = makeStyles(theme => ({
         minWidth: "21.5em", marginLeft: "1em", [theme.breakpoints.down("xs")]: {
             marginLeft: 0
         }
+    }, specialText: {
+        fontFamily: "Pacifico", color: theme.palette.common.orange
+    }, subtitle: {
+        marginBottom: "1em"
+    }, icon: {
+        marginLeft: "2em", [theme.breakpoints.down("xs")]: {
+            marginLeft: 0
+        }
+    }, serviceContainer: {
+        marginTop: "12em", [theme.breakpoints.down("sm")]: {
+            padding: 25
+        }
     }
 }))
 
 export function LandingPage() {
     const classes = useStyles();
     const theme = useTheme();
+    const matchesSM = useMediaQuery(theme.breakpoints.down("sm"));
 
     const defaultOptions = {
         loop: true, autoplay: true, animationData: animationData, rendererSettings: {
@@ -56,7 +65,7 @@ export function LandingPage() {
         }
     };
     return (<Grid container direction={"column"} className={classes.mainContainer}>
-        <Grid item>
+        <Grid item> {/*-----Hero Block-----*/}
             <Grid container justifyContent={"flex-end"} alignItems={"center"} direction={"row"}>
                 <Grid sm item className={classes.heroTextContainer}>
                     <Typography variant={"h2"} align={"center"}>
@@ -84,6 +93,38 @@ export function LandingPage() {
                 </Grid>
             </Grid>
         </Grid>
+        <Grid item>{/*-----Service Block-----*/}
+            <Grid container direction={"row"}
+                  justifyContent={matchesSM ? "center" : undefined}
+                  className={classes.serviceContainer}
+            >
+                <Grid item
+                      style={{
+                          marginLeft: matchesSM ? 0 : "5em", textAlign: matchesSM ? "center" : undefined
+                      }}>
+                    <Typography variant={"h4"}>
+                        Custom Software Development
+                    </Typography>
+                    <Typography variant={"subtitle1"} className={classes.subtitle}>
+                        Save Energy. Save Time. Save Money
+                    </Typography>
+                    <Typography variant={"subtitle1"}>
+                        Complete digital solutions, from investigation to {" "}
+                        <span className={classes.specialText}>celebration.</span>
+                    </Typography>
+                    <Button variant={"outlined"} className={classes.learnButton}>
+                        <span style={{marginRight: 10}}>Learn More</span>
+                        <ButtonArrow
+                            width={10}
+                            height={10}
+                            fill={theme.palette.common.blue}
+                        />
+                    </Button>
+                </Grid>
+                <Grid item>
+                    <img className={classes.icon} alt={"custom software icon"} src={customSoftwareIcon}/>
+                </Grid>
+            </Grid>
+        </Grid>
     </Grid>)
 }
-
--- src/components/ui/Theme.js
+++ src/components/ui/Theme.js
@@ -2,6 +2,8 @@ import {createTheme} from "@material-ui/core/styles";
 
 const arcBlue = "#0B72B9"
 const arcOrange = "#FFBA60"
+const arcGrey = "#868686"
+
 export default createTheme({
     palette: {
         common: {
@@ -18,6 +20,18 @@ export default createTheme({
             fontSize: "1rem", fontFamily: "Pacifico", textTransform: "none", color: "white"
         }, h2: {
             fontFamily: "Raleway", fontWeight: 700, fontSize: "2.5rem", color: arcBlue, lineHeight: 1.5
+        }, h4: {
+            fontFamily: "Raleway", fontSize: "1.75rem", color: `${arcBlue}`, fontWeight: 700
+        }, subtitle1: {
+            fontSize: "1.25rem", fontWeight: 300, color: arcGrey
+        }, learnButton: {
+            borderColor: arcBlue,
+            color: arcBlue,
+            borderWidth: 2,
+            textTransform: "none",
+            borderRadius: 50,
+            fontFamily: "Roboto",
+            fontWeight: "bold",
         }
     }
 })
