988955b79847303139d8dd88ca46f21cc054032b
Shahrooz Sabet <shahrooz.sabet@ist.com>
2022-05-25 16:02:56 +0200

    firebase func deployed.

--- functions/index.js
+++ functions/index.js
@@ -1,9 +1,32 @@
 const functions = require("firebase-functions");
+const config = functions.config();
+const admin = require("firebase-admin");
+const nodemailer = require("nodemailer");
+
+admin.initializeApp();
+
+const transporter = nodemailer.createTransport({
+  service: "gmail", auth: {
+    user: config.user.email, pass: config.user.password,
+  },
+});
+
+const mailOptions = {
+  from: "Shahrooz Development",
+  to: "shahrooz.sabet@gmail.com",
+  subject: "Testing nodemailer",
+  text: "Test successful",
+};
 
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
 //
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+exports.sendMail = functions.https.onRequest((request, response) => {
+  transporter.sendMail(mailOptions, (error) => {
+    if (error) {
+      response.send(error);
+    } else {
+      response.send("Message send successfully");
+    }
+  });
+});
