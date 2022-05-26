ec9dc73027100effec55a03a146442e7f1b809a3
Shahrooz Sabet <shahrooz.sabet@ist.com>
2022-05-26 15:40:42 +0200

    cors added.

--- functions/index.js
+++ functions/index.js
@@ -2,6 +2,7 @@ const functions = require("firebase-functions");
 const config = functions.config();
 const admin = require("firebase-admin");
 const nodemailer = require("nodemailer");
+const cors = require("cors")({origin: true});
 
 admin.initializeApp();
 
@@ -22,11 +23,13 @@ const mailOptions = {
 // // https://firebase.google.com/docs/functions/write-firebase-functions
 //
 exports.sendMail = functions.https.onRequest((request, response) => {
-  transporter.sendMail(mailOptions, (error) => {
-    if (error) {
-      response.send(error);
-    } else {
-      response.send("Message send successfully");
-    }
+  cors(request, response, ()=>{
+    transporter.sendMail(mailOptions, (error) => {
+      if (error) {
+        response.send(error);
+      } else {
+        response.send("Message send successfully");
+      }
+    });
   });
 });
